ad1c2c1d0f29ee25dddd826c00f48eda4d7aadc6
AlevtinaVyachina <86950608+AlevtinaVyachina@users.noreply.github.com>
2022-11-17 19:00:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('Я научился коммитить')
     print('Я научился пушить')
+    print('Коммит из гитхаба')
 
 if __name__ == '__main__':
     main()
